e43cc0ff2889f6fd87b498e79a9c34e65cd743c9
Serhii Mamedov <bboyvivus@gmail.com>
2021-02-25 23:45:40 +0200

    Add isPrimitive and isEqual functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,21 @@
+export const isPrimitive = (value: any) => (
+  typeof value !== "object" || value === null
+);
+
 export const copyValue = <T>(value: T): T => (
-  typeof value === "object" && value !== null
-    ? JSON.parse(JSON.stringify(value))
-    : value
+  isPrimitive(value) ? value : JSON.parse(JSON.stringify(value))
 );
 
 export const getUniqueId = () => (
   Math.random().toString(36).substr(2) + Date.now().toString(36)
 );
+
+export const isEqual = (firstVal: any, secondVal: any) => {
+  try {
+    return isPrimitive(firstVal)
+      ? firstVal === secondVal
+      : JSON.stringify(firstVal) === JSON.stringify(secondVal);
+  } catch (e) {
+    return false;
+  }
+};
